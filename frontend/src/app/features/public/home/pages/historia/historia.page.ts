9a8e2ae06e979e5834209b2fd52e72a267589055
JorgeDoicela <ismael02doicela@gmail.com>
2026-02-09 08:20:46 -0500

    Historia

--- frontend/src/app/features/public/home/pages/historia/historia.page.ts
+++ frontend/src/app/features/public/home/pages/historia/historia.page.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import {
-  IonHeader, IonToolbar, IonButtons, IonBackButton,
-  IonTitle, IonContent, IonCard, IonCardHeader,
-  IonCardTitle, IonCardContent, IonIcon
+  IonHeader, IonToolbar, IonButtons,
+  IonTitle, IonContent, IonIcon, IonButton
 } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import {
   shieldCheckmarkOutline, peopleOutline, ribbonOutline,
-  bodyOutline, businessOutline
+  bodyOutline, businessOutline, arrowBackOutline,
+  calendarOutline, medalOutline, sparklesOutline,
+  bookOutline, rocketOutline, globeOutline
 } from 'ionicons/icons';
 
 @Component({
@@ -19,10 +20,10 @@ import {
   styleUrls: ['./historia.page.scss'],
   standalone: true,
   imports: [
-    CommonModule, FormsModule,
-    IonHeader, IonToolbar, IonButtons, IonBackButton,
-    IonTitle, IonContent, IonCard, IonCardHeader,
-    IonCardTitle, IonCardContent, IonIcon
+    CommonModule,
+    IonHeader, IonToolbar, IonButtons,
+    IonTitle, IonContent, IonIcon, IonButton,
+    RouterLink
   ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
@@ -34,7 +35,14 @@ export class HistoriaPage implements OnInit {
       'people-outline': peopleOutline,
       'ribbon-outline': ribbonOutline,
       'body-outline': bodyOutline,
-      'business-outline': businessOutline
+      'business-outline': businessOutline,
+      'arrow-back-outline': arrowBackOutline,
+      'calendar-outline': calendarOutline,
+      'medal-outline': medalOutline,
+      'sparkles-outline': sparklesOutline,
+      'book-outline': bookOutline,
+      'rocket-outline': rocketOutline,
+      'globe-outline': globeOutline
     });
   }
 
