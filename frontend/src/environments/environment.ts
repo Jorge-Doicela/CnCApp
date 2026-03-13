d25561e931bdfd203e59b543293ad5e7f2a9b275
JorgeDoicela <ismael02doicela@gmail.com>
2026-03-13 09:52:46 -0500

    Conectarse a Railway

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',  // URL del backend Node.js
-  redirectUrl: 'http://localhost:8100/recuperar-password',
+  apiUrl: 'http://192.168.14.252:3000/api',  // URL del backend (IP Local de la PC)
+  redirectUrl: 'http://192.168.14.252:8100/recuperar-password',
 };
