d10c6a6c6e28e72a15f0f6959b96ff20d329b27b
JorgeDoicela <ismael02doicela@gmail.com>
2026-03-24 18:11:29 -0500

    Correción

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  // URL oficial del backend en Railway
-  apiUrl: 'https://cncapp-production.up.railway.app/api',
+  apiUrl: '/api',
   // Esta URL se usará para redirecciones, por defecto apunta al mismo dominio del frontend
   redirectUrl: window.location.origin + '/recuperar-password',
 };
