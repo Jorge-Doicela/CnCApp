6f47e9326902d1a20ae21ce74635818cfe445043
JorgeDoicela <ismael02doicela@gmail.com>
2026-04-22 17:10:31 -0500

    Home

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'ec.gob.cnc.app',
-  appName: 'CNC App',
+  appName: 'CNC',
   webDir: 'frontend/www'
 };
 
